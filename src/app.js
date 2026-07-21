07c8b0ea3dfaf0f747c53e75a547e56558f6c561
wilson <wilsonrueda2@gmail.com>
2026-07-20 21:21:04 -0500

    Add cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@ import productModule from "./modules/products/routes/product.routes.js";
 import authRoutes from "./modules/auth/routes/auth.routes.js";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 import documents from "./config/documentsApi.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 
 //Routes
 app.use("/api/v1/products", productModule);
